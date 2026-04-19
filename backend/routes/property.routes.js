161b3cf0f01628d463e8a7c15c41f3a4ae93cdef
martin <martin.kihahu@gmail.com>
2026-04-19 19:30:29 +0300

    Implement property model and routes on the backend

---
+++ backend/routes/property.routes.js
@@ -0,0 +1,54 @@
+import express from "express";
+import {
+  addProperty,
+  deleteProperty,
+  getAllProperties,
+  getMyProperties,
+  getPropertyCounts,
+  getPropertyDetails,
+  getSellerdashboard,
+  updateProperty,
+  updatePropertyStatus,
+} from "../controllers/property.controller.js";
+import { authorize, protect } from "../middleware/auth.middleware.js";
+
+const propertyRouter = express.Router();
+
+propertyRouter.get("/", getAllProperties);
+
+// Protected seller routes
+propertyRouter.post(
+  "/",
+  protect,
+  authorize("seller"),
+  upload.array("images", 10),
+  addProperty,
+);
+propertyRouter.get("/my", protect, authorize("seller"), getMyProperties);
+propertyRouter.put(
+  "/:id",
+  protect,
+  authorize("seller"),
+  upload.array("images", 10),
+  updateProperty,
+);
+
+propertyRouter.delete("/:id", protect, authorize("seller"), deleteProperty);
+propertyRouter.patch(
+  "/:id/status",
+  protect,
+  authorize("seller"),
+  updatePropertyStatus,
+);
+
+propertyRouter.get(
+  "/seller/dashboard",
+  protect,
+  authorize("seller"),
+  getSellerdashboard,
+);
+
+propertyRouter.get("/counts", getPropertyCounts);
+propertyRouter.get("/:id", getPropertyDetails);
+
+export default propertyRouter;
