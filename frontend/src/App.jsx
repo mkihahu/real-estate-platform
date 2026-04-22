23b8390109869bb476ac4c6ef2216d10ce9c824f
martin <martin.kihahu@gmail.com>
2026-04-23 02:13:58 +0300

    Implement register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/shared/LandingPage";
 import Properties from "./pages/shared/Properties";
 import PropertyDetails from "./pages/shared/PropertyDetails";
+import Register from "./pages/auth/Register";
 
 const HomePage = () => {
   return (
     <div>
       <Routes>
+        <Route path="/register" element={<Register />} />
+
         <Route path="/" element={<LandingPage />} />
         <Route path="/properties" element={<Properties />} />
         <Route path="/property/:id" element={<PropertyDetails />} />
