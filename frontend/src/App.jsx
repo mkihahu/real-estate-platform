83ccc2050cd1d65482f409f357e0f50140e9d340
martin <martin.kihahu@gmail.com>
2026-04-22 16:34:44 +0300

    Implement properties page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/shared/LandingPage";
+import Properties from "./pages/shared/Properties";
 
 const HomePage = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/properties" element={<Properties />} />
       </Routes>
     </div>
   );
